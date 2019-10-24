3f4ce31b931ac727fe09aecfb8b8cb1b6d878ef8
Kelby Mittan <kelbymittan@Kelbys-MacBook-Pro.local>
2019-10-24 10:40:52 -0400

    testing commits

---
+++ BlackJack/BlackJack/Suit.swift
@@ -0,0 +1,13 @@
+//
+//  Suit.swift
+//  BlackJack
+//
+//  Created by Kelby Mittan on 10/24/19.
+//  Copyright © 2019 Alex Paul. All rights reserved.
+//
+
+import Foundation
+
+enum Suit: String {
+    case diamond = "♦️"
+}
